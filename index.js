0ec101d926731759ee3bed50511c99914d1dc708
Akansha <57809427+a-k-a-n-s-h-a@users.noreply.github.com>
2020-07-10 00:24:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,18 +13,17 @@ var player2=random();
 
 console.log(player1,player2)
 
-var message="";
 if(player1>player2){
-    message ="Player1 wins";
+    document.querySelector("h1").innerHTML="🚩 Payer1 wins!"
 }
 else if(player2===player1){
-    message="Tie";
+    document.querySelector("h1").innerHTML="Draw!"
 
 }
 else{
-    message="Player2 wins";
+    document.querySelector("h1").innerHTML="Payer2 wins! 🚩"
 }
-document.getElementById("message").innerHTML=message;
+// document.getElementById("message").innerHTML=message;
 document.getElementsByClassName("dice");
 document.getElementsByClassName("img1")[0].src=url+arr[player1];
 document.getElementsByClassName("img2")[0].src=url+arr[player2];
